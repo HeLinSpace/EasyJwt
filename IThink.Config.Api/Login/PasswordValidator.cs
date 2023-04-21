b950ac9daaa621abc9b8d0e1c89c03e440bf54c9
helin <lin.he@datamind.tech>
2023-04-21 15:06:11 +0800

    1

--- IThink.Config.Api/Login/PasswordValidator.cs
+++ IThink.Config.Api/Login/PasswordValidator.cs
@@ -1,4 +1,5 @@
 ﻿using Easy.Jwt.Core;
+using System.Security.Claims;
 
 namespace IThink.Config.Api.Login
 {
@@ -6,8 +7,15 @@ namespace IThink.Config.Api.Login
     {
         public async Task<bool> ValidateAsync(PasswordValidationContext context)
         {
+            // do some thing 
+
+            context.CustomClaims = new List<System.Security.Claims.Claim>
+            {
+                new Claim(JwtClaimTypes.Id,"id")
+            };
             context.CustomResponse = new Dictionary<string, object> { { "status", 200 } };
-           return true;
+
+            return true;
         }
     }
 }
